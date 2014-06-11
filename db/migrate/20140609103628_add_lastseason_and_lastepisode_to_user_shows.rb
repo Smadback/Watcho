af73ba4a987dfed5edb5f3f3c565a08e0be31e85
Smadback <smadback@gmail.com>
2014-06-11 17:55:05 +0200

    Kaputte Migrationsdateien geloescht

--- db/migrate/20140609103628_add_lastseason_and_lastepisode_to_user_shows.rb
+++
@@ -1,9 +0,0 @@
-class AddLastseasonAndLastepisodeToUserShows < ActiveRecord::Migration
-  def change
-    
-    add_column('user_shows', 'lastseason', :integer)
-    add_column('user_shows', 'lastepisode', :integer)
-    add_column('user_shows', 'link', :string)
-    
-  end
-end
--- db/migrate/20140610073716_change_foreign_keys_in_user_shows.rb
+++
@@ -1,8 +0,0 @@
-class ChangeForeignKeysInUserShows < ActiveRecord::Migration
-  def change
-    
-    rename_column :user_shows, :user, :user_id
-    rename_column :user_shows, :show, :show_id
-    
-  end
-end
